8342d92ab73446e4c238f4772197e8edf874cb5f
Morre Thierry <thierry.morre@cognitic.be>
2022-03-23 08:39:48 +0100

    Ajoutez des fichiers projet.

---
+++ GestBank/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Models;
+
+List<Courant> comptes = new List<Courant>();
+
+Personne personne = new Personne()
+{
+    Nom = "Person",
+    Prenom = "Michael",
+    BirthDate = new DateTime(1981, 03, 17)
+};
+
+Courant courant = new Courant()
+{
+    Numero = "000001",
+    Titulaire = personne,
+    LigneDeCredit = 500
+};
+
+Courant courant2 = new Courant()
+{
+    Numero = "000002",
+    Titulaire = personne,
+    LigneDeCredit = 500
+};
+
+courant.Depot(500);
+courant2.Depot(500);
+courant.Retrait(700);
+Console.WriteLine(courant.Solde);
+
+comptes.Add(courant);
+comptes.Add(courant2);
+
+
+double total = 0;
+
+foreach (Courant c in comptes)
+{
+    if (c.Solde > 0)
+        total += c;
+}
+
+Console.WriteLine(total);
+
+Console.WriteLine(courant + courant2);
+
---
+++ Models/Courant.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Courant
+    {
+        private string _numero;
+        private double _solde;
+        private Personne _titulaire;
+        private double _ligneDeCredit;
+
+        public string Numero
+        {
+            get
+            {
+                return _numero;
+            }
+
+            set
+            {
+                _numero = value;
+            }
+        }
+
+        public double Solde
+        {
+            get
+            {
+                return _solde;
+            }
+
+            private set
+            {
+                _solde = value;
+            }
+        }
+
+        public Personne Titulaire
+        {
+            get
+            {
+                return _titulaire;
+            }
+
+            set
+            {
+                _titulaire = value;
+            }
+        }
+
+        public double LigneDeCredit
+        {
+            get
+            {
+                return _ligneDeCredit;
+            }
+
+            set
+            {
+                if (value < 0)
+                    return; //Exception
+
+                _ligneDeCredit = value;
+            }
+        }
+
+        public void Depot(double montant)
+        {
+            if (montant <= 0D)
+                return;
+
+            Solde += montant;
+        }
+
+        public void Retrait(double montant)
+        {
+            if (montant <= 0D)
+                return;
+
+            if (Solde - montant < -LigneDeCredit)
+                return;
+
+            Solde -= montant;
+        }
+    }
+}
